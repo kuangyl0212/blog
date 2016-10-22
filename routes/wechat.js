c311eb5033a06344b872811958b7b4a32f223621
ruling <rulingfong@outlook.com>
2016-10-22 21:37:29 +0800

    wechat

--- routes/wechat.js
+++ routes/wechat.js
@@ -12,39 +12,8 @@ var config = {
 router.get('/', wechat(config, function (req, res, next) {
     // 微信输入信息都在req.weixin上
     console.log('get wechat');
-    var message = req.weixin;
-    if (message.FromUserName === 'duchikai') {
-        // 回复屌丝(普通回复)
-        res.reply('hehe');
-    } else if (message.FromUserName === 'text') {
-        //你也可以这样回复text类型的信息
-        res.reply({
-            content: 'text object',
-            type: 'text'
-        });
-    } else if (message.FromUserName === 'kuangyulinok') {
-        // 回复一段音乐
-        res.reply({
-            type: "music",
-            content: {
-                title: "来段音乐吧",
-                description: "一无所有",
-                musicUrl: "http://mp3.com/xx.mp3",
-                hqMusicUrl: "http://mp3.com/xx.mp3",
-                thumbMediaId: "thisThumbMediaId"
-            }
-        });
-    } else {
-        // 回复高富帅(图文回复)
-        res.reply([
-            {
-                title: '你来我家接我吧',
-                description: '这是女神与高富帅之间的对话',
-                picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
-                url: 'http://nodeapi.cloudfoundry.com/'
-            }
-        ]);
-    }
+    // var message = req.weixin;
+    res.reply('Hello');
 }));
 
 module.exports = router;
